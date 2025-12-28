b9bf8fbfdb754e9197eab177cdeba94c46a38269
JoelLV <lovijoel2001@gmail.com>
2025-12-27 21:24:57 -0800

    Added boilerplate code for ro_editor widgets

---
+++ lib/ui/ro_editor/widgets/ro_editor_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RoEditor extends StatelessWidget {
+  const RoEditor({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

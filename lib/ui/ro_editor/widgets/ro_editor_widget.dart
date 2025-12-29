b47dc253ef32ef7380702d8506d6432a9e90ca32
JoelLV <lovijoel2001@gmail.com>
2025-12-29 13:30:54 -0700

    Begin button implementatio of toolbar

--- lib/ui/ro_editor/widgets/ro_editor_widget.dart
+++ lib/ui/ro_editor/widgets/ro_editor_widget.dart
@@ -5,6 +5,38 @@ class RoEditor extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        _ToolBar(),
+      ],
+    );
+  }
+}
+
+class _ToolBar extends StatelessWidget {
+  const _ToolBar();
+
+  @override
+  Widget build(BuildContext context) {
+    return IntrinsicWidth(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.surface,
+          boxShadow: [BoxShadow(blurRadius: 5)],
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Row(
+          mainAxisAlignment: .center,
+          children: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.delete_outline)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.swap_vert)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.copy)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.paste)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.edit_outlined)),
+          ],
+        ),
+      ),
+    );
   }
 }

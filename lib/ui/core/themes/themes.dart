88238b7241f953eee88634116ea44e3e53c5ee3e
JoelLV <lovijoel2001@gmail.com>
2025-12-29 13:30:21 -0700

    Refactored theme data to be in its own file.

---
+++ lib/ui/core/themes/themes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+final themeData = ThemeData(
+  // TODO: Temporary color scheme. Need to figure out what would be the final color scheme.
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue.shade50),
+);

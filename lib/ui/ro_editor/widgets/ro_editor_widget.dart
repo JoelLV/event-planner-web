ea8c0160ac61341777d76701354d0750f4df4448
JoelLV <lovijoel2001@gmail.com>
2025-12-30 23:40:13 -0700

    Implemented toolbar for Desktop-only view. Closing issue #6.

--- lib/ui/ro_editor/widgets/ro_editor_widget.dart
+++ lib/ui/ro_editor/widgets/ro_editor_widget.dart
@@ -5,38 +5,70 @@ class RoEditor extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        _ToolBar(),
-      ],
-    );
+    return const Column(children: [_ToolBar()]);
   }
 }
 
+/// Creates a desktop-only view toolbar for the RO editor.
 class _ToolBar extends StatelessWidget {
   const _ToolBar();
+
+  /// Generates a list of Icon buttons.
+  List<Widget> _getToolbarButtons(BuildContext context) {
+    // List of icons to generate.
+    const iconButtonData = <({IconData icon, String tooltip})>[
+      (icon: Icons.delete_outline, tooltip: 'Delete selected block/section'),
+      (icon: Icons.swap_vert, tooltip: 'Re-arrange blocks/sections'),
+      (icon: Icons.add, tooltip: 'Add block/section'),
+      (icon: Icons.copy, tooltip: 'Copy block/section'),
+      (icon: Icons.paste, tooltip: 'Paste block/section'),
+      (icon: Icons.remove_red_eye_outlined, tooltip: 'Toggle view mode'),
+    ];
+
+    return [
+      for (var (:icon, :tooltip) in iconButtonData)
+        Padding(
+          padding: const EdgeInsets.only(left: 15, right: 15),
+          // TODO: Implement actions for these buttons once they've been implemented.
+          child: _ToolbarButton(icon: icon, tooltip: tooltip, onPressed: () {}),
+        ),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
     return IntrinsicWidth(
-      child: Container(
-        decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.surface,
-          boxShadow: [BoxShadow(blurRadius: 5)],
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Row(
-          mainAxisAlignment: .center,
-          children: [
-            IconButton(onPressed: () {}, icon: Icon(Icons.delete_outline)),
-            IconButton(onPressed: () {}, icon: Icon(Icons.swap_vert)),
-            IconButton(onPressed: () {}, icon: Icon(Icons.add)),
-            IconButton(onPressed: () {}, icon: Icon(Icons.copy)),
-            IconButton(onPressed: () {}, icon: Icon(Icons.paste)),
-            IconButton(onPressed: () {}, icon: Icon(Icons.edit_outlined)),
-          ],
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 10, bottom: 10),
+          child: Row(
+            mainAxisAlignment: .center,
+            children: _getToolbarButtons(context),
+          ),
         ),
       ),
+    );
+  }
+}
+
+/// Helper widget class for the toolbar icon buttons.
+class _ToolbarButton extends StatelessWidget {
+  const _ToolbarButton({
+    required this.icon,
+    required this.tooltip,
+    required this.onPressed,
+  });
+
+  final IconData icon;
+  final String tooltip;
+  final void Function()? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: onPressed,
+      icon: Icon(icon, color: Theme.of(context).primaryColor),
+      tooltip: tooltip,
     );
   }
 }
